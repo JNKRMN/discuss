697eb6a091a515817651e3aa9993223f7e77db71
JNKRMN <jkeith@weedmaps.com>
2018-05-16 16:31:37 -0600

    [IDS-173] auth and oauth

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :discuss,
-  ecto_repos: [Discuss.Repo]
+config :discuss, ecto_repos: [Discuss.Repo]
 
 # Configures the endpoint
 config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "BHT/uUxn3s8Cz86uDQGPl5bQaWbKQDiCcaYonR1B+Vir1CrEyF/8HIxsL4ZvyL+T",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +22,13 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "b3281e92839f29da0279",
+  client_secret: "de04c823d7f3f0d5d81af867691e5f9183c7eed2"
